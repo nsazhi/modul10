d6d8b5871f70dc1c58421e3be4acf93bac4a103e
nsazhi <n190275@inbox.ru>
2024-09-26 15:29:40 +0400

    Add files via upload
    26/09/24

---
+++ module_10_5.py
@@ -0,0 +1,27 @@
+import multiprocessing
+from datetime import datetime
+
+
+def read_info(name):
+    all_data = []
+    with open(name) as file:
+        while file.readline():
+            all_data.append(file.readline())
+
+
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+# Линейный вызов
+start = datetime.now()
+for i in filenames:
+    read_info(i)
+end = datetime.now()
+print(end - start)
+
+# # Многопроцессный вызов
+# if __name__ == '__main__':
+#     start_m = datetime.now()
+#     with multiprocessing.Pool(processes=4) as pool:
+#         pool.map(read_info, filenames)
+#     end_m = datetime.now()
+#     print(end_m - start_m)
